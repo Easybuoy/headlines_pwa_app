882d1063c9d40a8eeaba593dc505d6bb6c5dc6ad
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-03-24 06:46:13 +0100

    [feature] added froneted

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+var path = require('path');
+
+var app = express();
+
+const port = process.env.PORT || 3000;
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/',function (req, res) {
+    res.sendFile(__dirname + '/public/index.html');
+    // console.log(__dirname, '/public');
+});
+
+
+app.listen(port,function () {
+    console.log('Server started at port ' +port);
+});
