91ab90112616d214c7a54151873a5462625ab1b1
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-03-25 01:27:34 +0100

    [feature]

--- api/newsapi.js
+++ api/newsapi.js
@@ -3,10 +3,49 @@ const newsapi = new NewsAPI('94446a2fa0e5422499caf4725c83e818');
 console.log(newsapi);
 
 
-newsapi.v2.topHeadlines({
-    // sources: 'bbc-news,the-verge',
-    q: 'lebron',
-    category: 'sports',
+var topHeadlines = (question, category) => {
+    newsapi.v2.topHeadlines({
+        // sources: 'bbc-news,the-verge',
+        q: 'lebron',
+        category: 'sports',
+        language: 'en',
+        country: 'us'
+    }).then(response => {
+        console.log(response);
+        /*
+          {
+            status: "ok",
+            articles: [...]
+          }
+        */
+    });
+};
+
+var everyThing = () => {
+newsapi.v2.everything({
+    q: 'bitcoin',
+    sources: 'bbc-news,the-verge',
+    domains: 'bbc.co.uk, techcrunch.com',
+    from: '2017-12-01',
+    to: '2017-12-12',
+    language: 'en',
+    sortBy: 'relevancy',
+    page: 1
+}).then(response => {
+    console.log(response);
+/*
+  {
+    status: "ok",
+    articles: [...]
+  }
+*/
+});
+
+};
+
+var sources = () => {
+newsapi.v2.sources({
+    category: 'technology',
     language: 'en',
     country: 'us'
 }).then(response => {
@@ -14,42 +53,10 @@ newsapi.v2.topHeadlines({
     /*
       {
         status: "ok",
-        articles: [...]
+        sources: [...]
       }
     */
 });
+};
 
-
-// newsapi.v2.everything({
-//     q: 'bitcoin',
-//     sources: 'bbc-news,the-verge',
-//     domains: 'bbc.co.uk, techcrunch.com',
-//     from: '2017-12-01',
-//     to: '2017-12-12',
-//     language: 'en',
-//     sortBy: 'relevancy',
-//     page: 2
-// }).then(response => {
-//     console.log(response);
-// /*
-//   {
-//     status: "ok",
-//     articles: [...]
-//   }
-// */
-// });
-
-
-// newsapi.v2.sources({
-//     category: 'technology',
-//     language: 'en',
-//     country: 'us'
-// }).then(response => {
-//     console.log(response);
-//     /*
-//       {
-//         status: "ok",
-//         sources: [...]
-//       }
-//     */
-// });
+module.exports = {topHeadlines, everyThing, sources};
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,19 @@
 $(document).ready(() => {
     $('#searchForm').on('submit', (e) => {
-        console.log($('#searchText').val());
+        let searchText = $('#searchText').val();
+        getMovies(searchText);
         e.preventDefault();
     });
 });
 
+
+function getMovies(searchText) {
+    console.log(searchText);
+    axios.get('https://newsapi.org/v2/articles?apiKey=94446a2fa0e5422499caf4725c83e818&q=' + searchText + '&category=sports')
+        .then((response) => {
+            console.log(response);
+            })
+        .catch((err) => {
+        console.log(err)
+        })
+}
