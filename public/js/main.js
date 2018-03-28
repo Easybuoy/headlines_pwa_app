0b6bf3988eb2cfd3b47dc8a975cc8c4b6c1b74a7
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-03-28 10:59:13 +0100

    [feature] headlines

--- public/js/main.js
+++ public/js/main.js
@@ -2,9 +2,11 @@ const APIKEY = '94446a2fa0e5422499caf4725c83e818';
 $(document).ready(() => {
     $('#searchForm').keyup( (e) => {
         let searchText = $('#searchText').val();
-        getNews(searchText);
+        getNewsBySearch(searchText);
         // console.log(searchText)
         e.preventDefault();
+
+
     });
 });
 
@@ -28,11 +30,29 @@ $(document).ready(() => {
 });
 
 
-function getNews(searchText) {
+function getNewsBySearch(searchText) {
     console.log(searchText);
     axios.get('https://newsapi.org/v2/everything?q='+searchText+'&apiKey='+APIKEY)
         .then((response) => {
-            console.log(response);
+            let news =  response.data.articles;
+// console.log(news)
+            let output = '';
+            $.each(news, (index, singlenews) => {
+                console.log(singlenews);
+                output += `
+
+            <div class="col-md-4">
+            <div class="card-body text-center">
+         <img class="card-image-top" src="${singlenews.urlToImage}">
+            <h5>${singlenews.title}</h5>
+            <a onclick="movieSelected('${singlenews.imdbID}')" class="btn btn-primary" href="#">Movie Details</a>
+
+            </div>
+            </div>
+          `;
+            });
+
+            $('#searchnews').html(output);
         })
         .catch((err) => {
             console.log(err)
