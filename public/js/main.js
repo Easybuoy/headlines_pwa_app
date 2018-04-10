944c70fbf6bef592a938cf8b5db0168f8002810f
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2018-04-10 09:59:44 +0100

    [feature] headline_app

--- public/js/main.js
+++ public/js/main.js
@@ -33,11 +33,15 @@ $(document).ready(() => {
 
 function getNewsBySearch(searchText) {
     $('#searchnews').empty();
-    console.log(searchText);
+
     axios.get('https://newsapi.org/v2/everything?q='+searchText+'&apiKey='+APIKEY)
         .then((response) => {
             let news =  response.data.articles;
+            if(news.length == 0){
+                noArticleFound();
 
+                return;
+            }
             this._dbPromise.then(function(db) {
                 if (!db) return;
 
@@ -45,7 +49,7 @@ function getNewsBySearch(searchText) {
                 var store = tx.objectStore('everything');
                 news.forEach(function (everything) {
                     store.put(everything);
-                })
+                });
 
             });
 
@@ -61,7 +65,7 @@ function getNewsBySearch(searchText) {
                 output += `
             <div class="col-lg-4 col-md-12 mb-4">
                 <div class="view overlay rounded z-depth-1">
-                    <img src="${singlenews.urlToImage}" class="img-fluid" alt="Sample project image" id="card-img" style="height: 250px; width: 100%;">
+                    <img src="${singlenews.urlToImage}" class="img-fluid" alt="${singlenews.title}" id="card-img" style="height: 250px; width: 100%;">
                     <a>
                         <div class="mask rgba-white-slight"></div>
                     </a>
@@ -72,7 +76,7 @@ function getNewsBySearch(searchText) {
                         <strong>${singlenews.title}</strong>
                     </h4>
                     <p class="grey-text">${trimmedDescription}</p>
-                    <a class="btn btn-indigo btn-sm" href="${singlenews.url}" target="_blank">
+                    <a class="btn btn-deep-orange btn-sm" href="${singlenews.url}" target="_blank">
                         <i class="fa fa-clone left"></i> View News</a>
                 </div>
             </div>
@@ -102,7 +106,11 @@ function getNewsBySources(){
     axios.get('https://newsapi.org/v2/sources?apiKey='+APIKEY)
         .then((response) => {
             let sources =  response.data.sources;
+            if(sources.length == 0){
+                noArticleFound();
 
+                return;
+            }
             this._dbPromise.then(function(db) {
                 if (!db) return;
 
@@ -120,10 +128,6 @@ function getNewsBySources(){
             let output = '';
             $.each(sources, (index, source) => {
                 console.log(source);
-                // var trimmedDescription = '';
-                // if(singlenews.description){
-                //     var trimmedDescription = singlenews.description.substr(0, 50);
-                // }
 
                 output += `
             <div class="col-lg-4 col-md-12 mb-4">
@@ -134,8 +138,59 @@ function getNewsBySources(){
                         <strong>${source.name}</strong>
                     </h4>
                      <p class="grey-text">${source.description}</p>
-                    <a class="btn btn-indigo btn-sm" href="${source.url}" target="_blank">
-                        <i class="fa fa-clone left"></i> View Source</a>
+                    <a class="btn btn-deep-orange btn-sm" onclick="getNewsBySourceHeadline('${source.id}')" href="#">
+                        <i class="fa fa-clone left"></i> View Source Headline</a>
+                </div>
+            </div>
+          `;
+            });
+            $('#searchnews').html(output);
+        })
+        .catch((err) => {
+            if (!err.response) {
+                // network error
+                errorSnackBar();
+            } else {
+
+            }
+        });
+}
+
+
+function getNewsBySourceHeadline(id){
+    $('#searchnews').empty();
+    axios.get('https://newsapi.org/v2/top-headlines?sources='+id+'&apiKey='+APIKEY)
+        .then((response) => {
+            let headlinesources =  response.data.articles;
+            if(headlinesources().length == 0){
+                noArticleFound();
+
+                return;
+            }
+
+            let output = '';
+            $.each(headlinesources, (index, headlinesource) => {
+                console.log(headlinesource);
+                var trimmedDescription = '';
+                if(headlinesource.description){
+                    var trimmedDescription = headlinesource.description.substr(0, 50);
+                }
+                output += `
+            <div class="col-lg-4 col-md-12 mb-4">
+                <div class="view overlay rounded z-depth-1">
+                    <img src="${headlinesource.urlToImage}" class="img-fluid" alt="${headlinesource.title}" id="card-img" style="height: 250px; width: 100%;">
+                    <a>
+                        <div class="mask rgba-white-slight"></div>
+                    </a>
+                </div>
+
+                <div class="card-body mt-3">
+                    <h4>
+                        <strong>${headlinesource.title}</strong>
+                    </h4>
+                    <p class="grey-text">${trimmedDescription}</p>
+                    <a class="btn btn-deep-orange btn-sm" href="${headlinesource.url}" target="_blank">
+                        <i class="fa fa-clone left"></i> View News</a>
                 </div>
             </div>
           `;
@@ -157,8 +212,12 @@ function getNewsByCountry(country){
     $('#searchnews').empty();
     axios.get('https://newsapi.org/v2/top-headlines?country='+country+'&apiKey='+APIKEY)
         .then((response) => {
-            // console.log(response);
             let countrynews =  response.data.articles;
+            if(countrynews.length == 0){
+                noArticleFound();
+
+                return;
+            }
             this._dbPromise.then(function(db) {
                 if (!db) return;
 
@@ -167,24 +226,20 @@ function getNewsByCountry(country){
                 var tx = db.transaction('countrynews', 'readwrite');
                 var store = tx.objectStore('countrynews');
                 countrynews.forEach(function (news) {
-                    console.log(news);
                     store.put(news);
                 })
 
             });
-
-
             let output = '';
             $.each(countrynews, (index, singlenews) => {
                 var trimmedDescription = '';
                 if(singlenews.description){
                     var trimmedDescription = singlenews.description.substr(0, 50);
                 }
-
-                output += `
+            output += `
             <div class="col-lg-4 col-md-12 mb-4">
                 <div class="view overlay rounded z-depth-1">
-                    <img src="${singlenews.urlToImage}" class="img-fluid" alt="Sample project image" id="card-img" style="height: 250px; width: 100%;">
+                    <img src="${singlenews.urlToImage}" class="img-fluid" alt="${singlenews.title}" id="card-img" style="height: 250px; width: 100%;">
                     <a>
                         <div class="mask rgba-white-slight"></div>
                     </a>
@@ -195,16 +250,17 @@ function getNewsByCountry(country){
                         <strong>${singlenews.title}</strong>
                     </h4>
                     <p class="grey-text">${trimmedDescription}</p>
-                    <a class="btn btn-indigo btn-sm" href="${singlenews.url}" target="_blank">
+                    <a class="btn btn-deep-orange btn-sm" href="${singlenews.url}" target="_blank">
                         <i class="fa fa-clone left"></i> View News</a>
                 </div>
             </div>
           `;
             });
-
             $('#searchnews').html(output);
+
         })
         .catch((err) => {
+        console.log(err)
             if (!err.response) {
                 // network error
                 errorSnackBar();
@@ -250,4 +306,17 @@ function openDatabase() {
                 });
         // }
     });
+}
+
+function noArticleFound() {
+    let output = '';
+    output = `
+ <div class="card-body mt-3">
+    <h4>
+    No Article Found
+    </h4>
+                 
+    </div>
+    `;
+    $('#searchnews').html(output);
 }
